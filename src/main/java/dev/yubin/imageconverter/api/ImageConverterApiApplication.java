ac4ccaaa09b3c92023d436e870f5f6fdd4d8580f
Yubin Shin <shin.yubin18@gmail.com>
2025-04-15 21:18:06 +0900

    init: initial Spring Boot project setup and Gradle dependencies added

---
+++ src/main/java/dev/yubin/imageconverter/api/ImageConverterApiApplication.java
@@ -0,0 +1,13 @@
+package dev.yubin.imageconverter.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageConverterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageConverterApiApplication.class, args);
+    }
+
+}
