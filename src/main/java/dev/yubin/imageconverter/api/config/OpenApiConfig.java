1dc640e74a2f99e7b73648abbc42e85e3598c5f6
river.yb.shin <river.yb.shin@openedges.com>
2025-05-17 23:33:02 +0900

    feat: open api config 재활성화

--- src/main/java/dev/yubin/imageconverter/api/config/OpenApiConfig.java
+++ src/main/java/dev/yubin/imageconverter/api/config/OpenApiConfig.java
@@ -1,12 +1,19 @@
-// package dev.yubin.imageconverter.api.config;
-//
-// import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-// import io.swagger.v3.oas.annotations.info.Info;
-// import io.swagger.v3.oas.annotations.servers.Server;
-// import org.springframework.context.annotation.Configuration;
-//
-// @OpenAPIDefinition(
-//    info = @Info(title = "Image Converter API", version = "v1"),
-////    servers = {@Server(url = "/api")})
-// @Configuration
-// public class OpenApiConfig {}
+package dev.yubin.imageconverter.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().info(new Info()
+			.title("Image Converter API")
+			.version("v1")
+			.description("Image to ASCII conversion API"));
+	}
+}
