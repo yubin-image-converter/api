a0719082e23b9c6314c7ec0b44f7d03004cf5636
river.yb.shin <river.yb.shin@openedges.com>
2025-05-18 09:53:38 +0900

    feat: swagger jwt추가 , try it out 불가하게

--- src/main/java/dev/yubin/imageconverter/api/config/OpenApiConfig.java
+++ src/main/java/dev/yubin/imageconverter/api/config/OpenApiConfig.java
@@ -2,6 +2,10 @@ package dev.yubin.imageconverter.api.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +14,24 @@ public class OpenApiConfig {
 
   @Bean
   public OpenAPI customOpenAPI() {
+
+    final String securitySchemeName = "JWT";
+
     return new OpenAPI()
         .info(
             new Info()
                 .title("Image Converter API")
                 .version("v1")
-                .description("Image to ASCII conversion API"));
+                .description("Image to ASCII conversion API"))
+        .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))  // 전체 API에 적용
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    securitySchemeName,
+                    new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
   }
 }
